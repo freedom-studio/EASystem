9a7c8d19747e79574c487e668f6efc063f53052b
Piotr Martyna <piotrek722@gmail.com>
2017-05-08 23:22:56 +0200

    Migration and Topology Interfaces

---
+++ migration-topology/src/main/java/pl/edu/agh/toik/migration/MigrationService.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.toik.migration;
+
+import pl.edu.agh.toik.Neighbour;
+
+import java.util.List;
+
+public interface MigrationService {
+    void emigrate(List<Neighbour> neighbours, Population population);
+    void immigrate(String agentID, Population population);
+    void setCommunicator(ICommunicator communicator);
+    void addMessageToBuffor(Message message);
+}
