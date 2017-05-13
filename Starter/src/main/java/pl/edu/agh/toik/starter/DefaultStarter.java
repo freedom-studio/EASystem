06cda629a27596de0ec407347922b9f74ec39bd2
Damian Kuś <damian.kus.main@gmail.com>
2017-05-13 21:10:01 +0200

    creating agentConfigs

--- Starter/src/main/java/pl/edu/agh/toik/starter/DefaultStarter.java
+++ Starter/src/main/java/pl/edu/agh/toik/starter/DefaultStarter.java
@@ -1,8 +1,11 @@
 package pl.edu.agh.toik.starter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -10,8 +13,11 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 import pl.edu.agh.lab.toik.comm.v2.INamingService;
 import pl.edu.agh.toik.Neighbour;
 import pl.edu.agh.toik.agent.Stage;
+import pl.edu.agh.toik.starter.mocks.MockId;
+import pl.edu.agh.toik.starter.mocks.agent.MockAgentConfig;
 import pl.edu.agh.toik.topology.TopologyFactory;
 import pl.edu.agh.toik.topology.TopologyType;
+import pl.edu.agh.toik.worker.AgentConfig;
 import pl.edu.agh.toik.worker.WorkerConfig;
 
 public class DefaultStarter implements Starter {
@@ -39,6 +45,32 @@ public class DefaultStarter implements Starter {
 			workerAgentMap.put(id, namingService.GetAgents(id));
 		}
 		
-		Map<String, List<Neighbour>> agentToNeighbour = topologyFactory.createTopology(topologyType, workerAgentMap);
+		Map<String, List<Neighbour>> agentToNeighbours = topologyFactory.createTopology(topologyType, workerAgentMap);
+		Map<String, List<AgentConfig>> workerToAgentConfigs = new HashMap<>();
+		Map<String, WorkerConfig> workerIdToConfig = new HashMap<>();
+		
+		for (WorkerConfig config : workerConfigs) {
+			workerIdToConfig.put(config.getId().getValue(), config);
+		}
+		
+		for (Entry<String, List<String>> agents : workerAgentMap.entrySet()) {
+			String workerId = agents.getKey();
+			List<AgentConfig> agentConfigs = new ArrayList<>();
+			
+			for (String agentId : agents.getValue()) {
+				agentConfigs.add(new MockAgentConfig(
+						new MockId(agentId), 
+						agentToNeighbours.get(agentId)
+							.stream()
+							.map(n -> new MockId(n.getAgentID()))
+							.collect(Collectors.toList()),
+						stages));
+			}
+			
+			workerIdToConfig.get(workerId)
+				.getAgentConfigs()
+				.addAll(agentConfigs);
+			workerToAgentConfigs.put(workerId, agentConfigs);
+		}
 	}
 }
