b2d9580fef0af75d0043a7597e2493368dfb0b94
Damian Kuś <damian.kus.main@gmail.com>
2017-05-15 18:20:01 +0200

    Added ID retrieval to Worker Config

--- Worker/src/main/java/pl/edu/agh/toik/worker/WorkerConfig.java
+++ Worker/src/main/java/pl/edu/agh/toik/worker/WorkerConfig.java
@@ -1,10 +1,12 @@
 package pl.edu.agh.toik.worker;
 
-import pl.edu.agh.toik.observers.AgentObserver;
-
 import java.util.List;
 
+import pl.edu.agh.toik.communication.Id;
+import pl.edu.agh.toik.observers.AgentObserver;
+
 public interface WorkerConfig {
+	Id getId();
     StopStrategy getStopStrategy();
     List<AgentObserver> getObservers();
     List<AgentConfig> getAgentConfigs();
